97062021bdf1ff2a0f8a6fd9dc99053da2603e0a
Mahmoud Abdalhamid <mahmoudabdalhamid013@gmail.com>
2024-11-16 01:15:46 +0200

    Add files via upload

---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class AgeCalc : Form
+    {
+        public AgeCalc()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+            int age;
+            age = Convert.ToInt16(textBox1.Text);
+            int calc = 2024 - age;
+            label2.Text = Convert.ToString(calc);
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
